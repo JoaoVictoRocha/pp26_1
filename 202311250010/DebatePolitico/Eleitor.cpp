db103cf6a84bc1dc17c39887072c198f5ce4a928
João Rocha <141365688+JoaoVictoRocha@users.noreply.github.com>
2026-05-20 06:44:02 -0300

    Delete 202311250010/DebatePolitico directory

--- 202311250010/DebatePolitico/Eleitor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Eleitor.h"
-
-Eleitor::Eleitor(const std::string& nome) {
-    this->nome = nome;
-}
-
-void Eleitor::atualizar(const std::string& mensagem) {
-    std::cout << "[NOTIFICACAO CELULAR - " << nome << "]: " << mensagem << std::endl;
-}
